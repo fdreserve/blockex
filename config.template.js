474e5738ad25e965e1789f84fb1c7127090dae50
FD Reserve Core <42116303+fdreserve@users.noreply.github.com>
2022-03-16 00:41:37 +0100

    Update config.template.js

--- config.template.js
+++ config.template.js
@@ -71,7 +71,7 @@ const config = {
   ],
 
   freegeoip: {
-    api: 'https://extreme-ip-lookup.com/json/', //@todo need to find new geoip service as the limits are too small now (hitting limits) 
+    api: 'https://api.freegeoip.app/json/', //@todo need to find new geoip service as the limits are too small now (hitting limits) 
     key: 'XXXXXXXXXXXXXXXXX' //API Key 
   },
   coinMarketCap: {
