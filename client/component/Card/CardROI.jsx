96ac2323c95a9de79f65ff674783703d36a29d35
FD Reserve Core <42116303+fdreserve@users.noreply.github.com>
2022-03-18 07:58:18 +0100

    Update CardROI.jsx

--- client/component/Card/CardROI.jsx
+++ client/component/Card/CardROI.jsx
@@ -13,12 +13,6 @@ const CardROI = ({ coin, supply }) => {
   const sns = coin.snOn;
   const cns = coin.cnOn;
   const rns = coin.rnOn;
-  const snsubsidy = blockchain.getSnSubsidy(coin.blocks);
-  const cnsubsidy = blockchain.getCnSubsidy(coin.blocks);
-  const rnsubsidy = blockchain.getRnSubsidy(coin.blocks);
-  const snroi = blockchain.getSnROI(snsubsidy, sns);
-  const cnroi = blockchain.getCnROI(cnsubsidy, cns);
-  const rnroi = blockchain.getRnROI(rnsubsidy, rns);
   const totalsupplynft = ((config.nftNodes.Rnodes * config.coinDetails.reservenodeCollateral) + (config.nftNodes.Cnodes * config.coinDetails.cashnodeCollateral) + (config.nftNodes.Snodes * config.coinDetails.securenodeCollateral));
 
   
@@ -64,53 +58,7 @@ const CardROI = ({ coin, supply }) => {
           Market Cap USD :
         </div>
         <div className="h5">
-          {numeral(supply.t * coin.usd).format('0,0.00')} USD
-        </div>
-      </div>
-      <div className="mb-3">
-        <div className="h5">
-          Market Cap BTC
-        </div>
-        <div className="h5">
-          {numeral(supply.t * coin.btc).format('0,0.0000')} BTC
-        </div>
-      </div>
-      <div className="mb-3">
-        <div className="h5">
-          Secure Node Worth :
-        </div>
-        <div className="h5">
-        {numeral(mncoins * coin.usd).format('0,0.00')} USD
-        </div>
-        <div className="h6">
-          Estimated ROI
-        </div>
-        <div className="h5">
-          {numeral(snroi).format('0,0.0000')}%
-        </div>
-        <div className="h5">
-          Cash Node Worth :
-        </div>
-        <div className="h5">
-        {numeral(config.coinDetails.cashnodeCollateral * coin.usd).format('0,0.00')} USD
-        </div>
-        <div className="h6">
-          Estimated ROI
-        </div>
-        <div className="h5">
-          {numeral(cnroi).format('0,0.0000')}%
-        </div>
-        <div className="h5">
-          Reserve Node Worth :
-        </div>
-        <div className="h5">
-        {numeral(config.coinDetails.reservenodeCollateral * coin.usd).format('0,0.00')} USD
-        </div>
-        <div className="h6">
-          Estimated ROI
-        </div>
-        <div className="h5">
-          {numeral(rnroi).format('0,0.0000')}%
+          {numeral(supply.t * coin.usd).format('0,0.00')} $ - {numeral(supply.t * coin.btc).format('0,0.000')} BTC
         </div>
       </div>
     </Card>
